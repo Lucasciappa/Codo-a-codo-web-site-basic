f52a2f2b8e45a8e2f8b7dbc815c42088f297a790
lucasciappa <lucasciappa97@gmail.com>
2021-05-09 20:22:06 -0300

    update panel-aside and section

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 /*   *** MENU ***  */
 
 
+const d = document,
+    w = window;
+
+
 ((d) => {
     const $btnMenu = d.querySelector(".menu-boton"),
-     $nav = d.querySelector(".nav");
+        $nav = d.querySelector(".nav");
 
     $btnMenu.addEventListener("click", (e) => {
         $btnMenu.firstElementChild.classList.toggle("none");
@@ -12,4 +16,83 @@
     });
 
 
-})(document);
+})(d);
+
+
+
+
+
+
+d.addEventListener("DOMContentLoaded", e => {
+
+    scrollTopButtom(".scroll-top-btn");
+    scrollSpy()
+
+
+
+});
+
+
+function scrollTopButtom(btn) {
+    const $scrollBtn = d.querySelector(btn);
+
+
+    w.addEventListener("scroll", e => {
+        let scrollTop = w.pageYOffset || d.documentElement.scrollTop;
+
+        if (scrollTop > 1000) {
+            $scrollBtn.classList.remove("hidden");
+        } else {
+            $scrollBtn.classList.add("hidden");
+
+        }
+
+        // console.log(w.pageYOffset, d.documentElement.scrollTop );
+    })
+
+    d.addEventListener("click", e => {
+        if (e.target.matches(btn)) {
+            w.scrollTo({
+                behavior: "smooth",
+                top: 0,
+                // left:0
+            })
+        }
+    })
+
+}
+
+
+
+
+function scrollSpy() {
+
+    const $sections = d.querySelectorAll("section[data-scroll-spy]")
+
+    const cb = (entries) => {
+        // console.log("entries", entries);
+
+        entries.forEach(entry => {
+            // console.log("entry",entry);
+            const id = entry.target.getAttribute("id");
+            if (entry.isIntersecting) {
+                d.querySelector(`a[data-scroll-spy][href="#${id}"]`).classList.add("active");
+
+            } else {
+                d.querySelector(`a[data-scroll-spy][href="#${id}"]`).classList.remove("active");
+            }
+        });
+    }
+
+    const observer = new IntersectionObserver(cb, {
+        // root: 
+        // rootMargin: "-350px",
+          threshold: [0.333, 0.75]
+    });
+
+    $sections.forEach(el => observer.observe(el))
+
+}
+
+
+
