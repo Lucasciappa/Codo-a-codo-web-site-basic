3a9f86546e42f165eeda770c410310020a41bd9c
lucasciappa <lucasciappa97@gmail.com>
2021-05-12 23:47:14 -0300

    elements order

--- script.js
+++ script.js
@@ -26,7 +26,8 @@ const d = document,
 d.addEventListener("DOMContentLoaded", e => {
 
     scrollTopButtom(".scroll-top-btn");
-    scrollSpy()
+    scrollSpy();
+    contactFormValidations();
 
 
 
@@ -91,6 +92,51 @@ function scrollSpy() {
     });
 
     $sections.forEach(el => observer.observe(el))
+
+}
+
+
+
+
+
+
+function contactFormValidations(){
+    const $form = d.querySelector(".form"),
+     $inputs = d.querySelectorAll(".form [required]");
+    
+
+
+    $inputs.forEach((input) => {
+        const $span = d.createElement("span");
+        $span.id = input.name;
+        $span.textContent = input.title;
+        $span.classList.add("form-error", "none")
+        input.insertAdjacentElement("afterend", $span);
+
+    });
+
+    d.addEventListener("keyup", e=>{
+        if(e.target.matches(".form [required]")){
+            let $input = e.target,
+             pattern = $input.pattern || $input.dataset.pattern;
+        // console.log($input,pattern)
+        if(pattern && $input.value !== ""){
+            let regex = new RegExp(pattern);
+            return !regex.exec($input.value)
+             ? d.getElementById($input.name).classList.add("is-active")
+             : d.getElementById($input.name).classList.remove("is-active")
+        }
+        
+        if (!pattern){
+            return $input.value === ""
+             ? d.getElementById($input.name).classList.add("is-active")
+             : d.getElementById($input.name).classList.remove("is-active")
+
+        }
+    }
+
+
+    });
 
 }
 
